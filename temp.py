3f4a1156f0b2739d4ad5678ed70d23192e283b67
xz339 <XZ@XIaos-MacBook-Pro.local>
2016-09-08 19:48:42 -0400

    nnn

---
+++ temp.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+
+This is a temporary script file.
+"""
+
+
+def translate():
+    english = input('Please enter English word:')
+
+  
+
+    if english =='merry':
+         swedish = 'god'
+ 
+    if english =='christmas':
+         swedish = 'jul'
+
+    if english =='and':
+         swedish = 'och'
+     
+    if english =='happy':
+         swedish = 'gott'
+         
+    if english =='new':
+         swedish = 'nytt'
+         
+    if english =='year':
+         swedish = 'år'
+
+    print(swedish)
+    return swedish
+    
