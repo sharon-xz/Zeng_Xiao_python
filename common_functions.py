ed0da3dc3c48b8de4128e21b85eacb6832fe6cff
xz339 <xz339@georgetown.edu>
2018-01-19 18:49:08 -0500

    new content
    add new content

---
+++ common_functions.py
@@ -0,0 +1,26 @@
+#common python functions
+
+test_str = "ewhfuwihfuuwirhfuirh"
+test_list = [1,2,3]
+
+len(test_str)
+# for loop, don't len(string)-1, automatically -1
+
+null = None
+
+
+for i in range(3):
+    print (i,"\n")
+
+for i in range(1,10,2):
+    print (i)
+#13579
+
+#0,1,2  range(start, stop,step)
+    
+if (test_str=="yyyy") or (test_str=="fwfr"):
+    print("no")
+    
+#Add an element to a list
+test_list.append("3333")
+print(test_list)
